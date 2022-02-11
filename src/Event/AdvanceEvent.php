c832c08bcae470de59ed11420401a610d07e5aec
Samaël Villette <villette.samael@gmail.com>
2022-02-11 10:41:01 +0100

    feat: added events and event listener

---
+++ src/Event/AdvanceEvent.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symandy\Component\ProgressEvent\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+final class AdvanceEvent extends Event
+{
+
+    private ?int $step;
+
+    public function __construct(int $step = 1)
+    {
+        $this->step = $step;
+    }
+
+    public function getStep(): int
+    {
+        return $this->step;
+    }
+
+}
---
+++ src/Event/FinishEvent.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symandy\Component\ProgressEvent\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+final class FinishEvent extends Event
+{
+
+}
